2d660b1b769b542615baefe1875c6944ae27dea4
ljcui1 <yangyangjiaocui@gmail.com>
2024-05-26 16:48:09 -0700

    set up non working ui and cursor image

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+
+    [SerializeField] private Texture2D hand;
+    [SerializeField] private Button treat;
+    [SerializeField] private Button tickle;
+    [SerializeField] private Slider rat1;
+    [SerializeField] private Slider rat2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.SetCursor(hand, Vector2.zero, CursorMode.Auto);
+        Cursor.visible = true;
     }
 
     // Update is called once per frame
