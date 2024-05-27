fdb0dfce49675b7ec48fa6e082043294f5cf4bd7
ljcui1 <yangyangjiaocui@gmail.com>
2024-05-27 13:45:02 -0700

    starting rat code

---
+++ Assets/Rat.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using RenownedGames.AITree;
+using UnityEngine;
+
+public class Rat : MonoBehaviour
+{
+    [SerializeField] private Cursor cursor;
+    [SerializeField] private BehaviourRunner br;
+    [SerializeField] private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
